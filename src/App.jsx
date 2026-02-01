2bece77f629e05c7ee426404cbed79533939b39d
zeyadhost <zbekhet@com.svu.edu.eg>
2026-02-01 18:25:31 +0200

    created the React app with Vite and added main components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>hi from the scariest bakery ever</h1>
+            <p>do not touch ANY sorts of food, even samples will result in your death hahahahahhahah</p>
+        </div>
+    )
+}
+
+export default App
